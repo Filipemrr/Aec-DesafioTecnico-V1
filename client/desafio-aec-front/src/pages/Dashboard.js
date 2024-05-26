2d404cd0dfd026fa444e85441327dadfce22965c
filipemmr <112185872+Filipemrr@users.noreply.github.com>
2024-05-26 00:12:16 -0300

    CHORE: Adicionando tabela

--- client/desafio-aec-front/src/pages/Dashboard.js
+++ client/desafio-aec-front/src/pages/Dashboard.js
@@ -1,17 +1,42 @@
-import Navbar from '../components/Navbar';
-import { Grid, useMediaQuery } from '@mui/material';
 import React from 'react';
-
-function Dashboard() {
-  const isSmallScreen = useMediaQuery('(max-width: 900px)');
-
+import { CssVarsProvider } from '@mui/joy/styles';
+import CssBaseline from '@mui/joy/CssBaseline';
+import Box from '@mui/joy/Box';
+import Button from '@mui/joy/Button';
+import Typography from '@mui/joy/Typography';
+import DownloadRoundedIcon from '@mui/icons-material/DownloadRounded';
+import '../index.css';
+import BasicTable from '../components/Table.js';
+export default function Dashboard() {
   return (
-    <Grid container>
-      <Navbar name="Aec-Desafio" cor="#017BF7" />
-      <Grid container className="LoginView-container">
-      </Grid>
-    </Grid>
+    <CssVarsProvider disableTransitionOnChange>
+      <CssBaseline />
+      <Box sx={{ display: 'flex', minHeight: '100dvh' }}>
+        <Box
+          component="main"
+          className="MainContent"
+        >
+          <Box
+            className="ContentBox"
+          >
+            <Typography level="h2" component="h1">
+              Endereço
+            </Typography>
+            <Button
+              color="primary"
+              startDecorator={<DownloadRoundedIcon />}
+              size="sm"
+            >
+              Download CSV
+            </Button>
+          </Box>
+          <Box
+          className='TableContent'
+          >
+          <BasicTable/>
+          </Box>
+        </Box>
+      </Box>
+    </CssVarsProvider>
   );
 }
-
-export default Dashboard;
